70cfb2afe78ba70f4e0bdce994d2100aea765194
moonsemi <48460923+moonsemi@users.noreply.github.com>
2020-04-20 00:37:22 +0900

    JS App만들기 예제.js
    - clock 만들기(setInterval 사용)
    - 유저 name을 input에서 가져와서 local storage에 저장해서 보여주기

--- gretting.js
+++ gretting.js
@@ -5,6 +5,26 @@ greeting = document.querySelector(".js-greeting");
 const USER_LS = "currentUser",
 SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+};
+
+function handleSubmit(e){
+    // submit시 새로고침 방지코드
+    e.preventDefault();
+    // 사용자가 넣을 input 값
+    const currentValue = input.value;
+    // "Hello currentValue" 부분 함수 호출
+    paintGreeting(currentValue);
+    // 로컬스토리지에 currentValue 저장 함수 호출
+    saveName(currentValue);
+};
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+};
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -15,7 +35,7 @@ function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     // nobody
     if(currentUser === null){
-
+        askForName();
     }else{
         paintGreeting(currentUser);
     };
@@ -23,6 +43,6 @@ function loadName(){
 
 function init(){
     loadName();
-}
+};
 
 init();
